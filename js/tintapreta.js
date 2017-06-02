8636e0c20ce2f50df07c8a73b335e92539bf56cb
Douglas Gusson <douglasgusson@gmail.com>
2017-06-02 20:21:10 -0300

    acertos na aparencia e capas do album

--- js/tintapreta.js
+++ js/tintapreta.js
@@ -6,6 +6,7 @@ var $botaoShuffle = document.querySelector('#shuffle-music');
 var $botaoDownload = document.querySelector('#download-music');
 
 var $playerAudio = document.querySelector('#player');
+var $capaAlbum = document.querySelector('#capa-album');
 var $fileMp3 = document.querySelector('#file-mp3');
 var $canvas = document.getElementById('canvas-barra');
 
@@ -17,23 +18,23 @@ var musicaAtual = 0;
 var flagShuffle = false;
 
 var musicas = [
-    {'id':'1', 'nome':'Não morre tão cedo', 'artista':'Noel Rosa'},
-    {'id':'2', 'nome':'Minha viola', 'artista':'Noel Rosa'},
-    {'id':'3', 'nome':'Com que roupa', 'artista':'Noel Rosa'},
-    {'id':'4', 'nome':'Gago apaixonado', 'artista':'Noel Rosa'},
-    {'id':'5', 'nome':'Riso de criança', 'artista':'Noel Rosa'},
-    {'id':'6', 'nome':'Fita amarela', 'artista':'Noel Rosa'},
-    {'id':'7', 'nome':'Meu barracão', 'artista':'Noel Rosa'},
-    {'id':'8', 'nome':'Não tem tradução', 'artista':'Noel Rosa'},
-    {'id':'9', 'nome':'Três apitos', 'artista':'Noel Rosa'},
-    {'id':'10', 'nome':'Rapaz folgado', 'artista':'Noel Rosa'},
-    {'id':'11', 'nome':'Capricho de rapaz solteiro', 'artista':'Noel Rosa'},
-    {'id':'12', 'nome':'Dama do cabaré', 'artista':'Noel Rosa'},
-    {'id':'13', 'nome':'Conversa de botequim', 'artista':'Noel Rosa'},
-    {'id':'14', 'nome':'Palpite infeliz', 'artista':'Noel Rosa'},
-    {'id':'15', 'nome':'Só pode ser você', 'artista':'Noel Rosa'},
-    {'id':'16', 'nome':'Eu sei sofrer', 'artista':'Noel Rosa'},
-    {'id':'17', 'nome':'Último desejo', 'artista':'Noel Rosa'}
+    {'id':'1', 'nome':'Não morre tão cedo', 'artista':'Noel Rosa', 'capa':'Vol_7.jpg'},
+    {'id':'2', 'nome':'Minha viola', 'artista':'Noel Rosa', 'capa':'Vol_1.jpg'},
+    {'id':'3', 'nome':'Com que roupa', 'artista':'Noel Rosa', 'capa':'Vol_1.jpg'},
+    {'id':'4', 'nome':'Gago apaixonado', 'artista':'Noel Rosa', 'capa':'Vol_1.jpg'},
+    {'id':'5', 'nome':'Riso de criança', 'artista':'Noel Rosa', 'capa':'Vol_4.jpg'},
+    {'id':'6', 'nome':'Fita amarela', 'artista':'Noel Rosa', 'capa':'Vol_7.jpg'},
+    {'id':'7', 'nome':'Meu barracão', 'artista':'Noel Rosa', 'capa':'Vol_4.jpg'},
+    {'id':'8', 'nome':'Não tem tradução', 'artista':'Noel Rosa', 'capa':'Vol_4.jpg'},
+    {'id':'9', 'nome':'Três apitos', 'artista':'Noel Rosa', 'capa':'Vol_6.jpg'},
+    {'id':'10', 'nome':'Rapaz folgado', 'artista':'Noel Rosa', 'capa':'Vol_6.jpg'},
+    {'id':'11', 'nome':'Capricho de rapaz solteiro', 'artista':'Noel Rosa', 'capa':'Vol_3.jpg'},
+    {'id':'12', 'nome':'Dama do cabaré', 'artista':'Noel Rosa', 'capa':'Vol_5.jpg'},
+    {'id':'13', 'nome':'Conversa de botequim', 'artista':'Noel Rosa', 'capa':'Vol_5.jpg'},
+    {'id':'14', 'nome':'Palpite infeliz', 'artista':'Noel Rosa', 'capa':'Vol_5.jpg'},
+    {'id':'15', 'nome':'Só pode ser você', 'artista':'Noel Rosa', 'capa':'Vol_5.jpg'},
+    {'id':'16', 'nome':'Eu sei sofrer', 'artista':'Noel Rosa', 'capa':'Vol_5.jpg'},
+    {'id':'17', 'nome':'Último desejo', 'artista':'Noel Rosa', 'capa':'Vol_6.jpg'}
 ];
 
 loadPlaylist();
@@ -105,6 +106,7 @@ function loadMusic(index) {
     musicaAtual = index;
     $nomeMusica.textContent = musicas[index].nome;
     $artista.textContent = musicas[index].artista;
+    $capaAlbum.setAttribute('src', 'img/' + musicas[index].capa);
     $botaoDownload.setAttribute('href', 'musics/'+ musicas[index].id +'.mp3');
     $botaoDownload.setAttribute('download', musicas[index].nome +'.mp3');
     $fileMp3.setAttribute('src', 'musics/'+ musicas[index].id +'.mp3');
